11ec1045c8978077e8897bd87a8e4d03c4ae1eea
ka89801-source <ka89801@gmail.com>
2026-04-27 20:15:50 +0300

    Update login.js

--- api/login.js
+++ api/login.js
@@ -8,7 +8,7 @@ module.exports = async function handler(req, res) {
   try {
     const supabase = createClient(
       process.env.SUPABASE_URL,
-      process.env.SUPABASE_ANON_KEY
+      process.env.SUPABASE_SERVICE_ROLE_KEY
     );
 
     const { full_name, phone } = req.body || {};
