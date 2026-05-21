7a047fdabfde97755d1530511592e5185ff4c788
ka89801-source <ka89801@gmail.com>
2026-05-21 09:34:05 +0300

    Update najiz-service.js

--- api/najiz-service.js
+++ api/najiz-service.js
@@ -14,9 +14,20 @@ function getFieldValue(field) {
 }
 
 export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  const allowedOrigins = [
+  "https://www.araf.online",
+  "https://www.araf.online"
+];
+
+const origin = req.headers.origin;
+
+if (allowedOrigins.includes(origin)) {
+  res.setHeader("Access-Control-Allow-Origin", origin);
+}
+
+res.setHeader("Vary", "Origin");
+res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
